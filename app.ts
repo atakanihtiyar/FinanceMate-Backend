01ba623902bb3e28c530790f244bfe7ca73fe4b9
ati <26187453+atakanihtiyar@users.noreply.github.com>
2024-04-16 16:27:37 +0300

    add project dependencies and set configs

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 5050
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+try {
+	app.get('/', (req, res) => {
+		res.send('Hello World!')
+	})
+
+	app.listen(PORT, () => {
+		return console.log(`Server listening at http://localhost:${PORT}`)
+	})
+}
+catch (err) {
+	console.log(err)
+}
